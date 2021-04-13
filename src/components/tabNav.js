e6c6832e476910efb08092617802f92b98914d20
rylieohashi <xxryliex@gmail.com>
2021-04-12 23:45:19 -0400

    tabnav grid styles

--- src/components/tabNav.js
+++ src/components/tabNav.js
@@ -4,7 +4,15 @@ class TabNav extends Component {
     render() {
         return (
             <div className='tab-nav'>
-
+                <div className='tab-nav__tabs'>
+                    {
+                        this.props.tabs.map((tab, index) => {  //we want to go into the tab and render the components
+                            return <a className='tab-nav__tab'>{tab.title}</a>
+                        })
+                    }
+                </div>
+                <div>requests or newsletter content goes here</div>
+                
             </div>
         )
 
